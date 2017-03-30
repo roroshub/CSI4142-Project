4273e9bda548bfaf5a64373f020c7450f51dacd3
Grégoire <gregonymous@gmail.com>
2017-03-30 10:25:21 -0400

    First draft of the SQL schema, w/ Location and ProductPrice

---
+++ sql_schema.sql
@@ -0,0 +1,30 @@
+DROP SCHEMA IF EXISTS csi4142project CASCADE;
+CREATE SCHEMA csi4142project;
+		
+
+DROP TABLE IF EXISTS csi4142project.Location;
+CREATE TABLE IF NOT EXISTS  csi4142project.Location (
+		location_key	SERIAL	PRIMARY KEY,
+		location_type	VARCHAR(50)	NOT NULL,
+		city	 		VARCHAR(30)	NOT NULL,
+		country 		VARCHAR(30)	NOT NULL,
+		gdp		 		INTEGER		NOT NULL,
+		population 		INTEGER		NOT NULL,
+		life-expectancy	INTEGER		NOT NULL,
+		anav_income		DOUBLE		NOT NULL);
+		
+		
+/*	Add Date & Product here	*/
+
+
+DROP TABLE IF EXISTS csi4142project.ProductPrice;
+CREATE TABLE IF NOT EXISTS  csi4142project.ProductPrice (
+		pp_key			INTEGER		NOT NULL,
+		date_key		INTEGER		NOT NULL,
+		product_key		INTEGER		NOT NULL,
+		location_key	INTEGER		NOT NULL,
+		price 			DOUBLE		NOT NULL,
+		PRIMARY KEY (pp_key), 
+		FOREIGN KEY(date_key) REFERENCES csi4142project.Date(date_key) ON DELETE RESTRICT ON UPDATE CASCADE, 
+		FOREIGN KEY(product_key) REFERENCES csi4142project.Product(product_key) ON DELETE RESTRICT ON UPDATE CASCADE, 
+		FOREIGN KEY(location_key) REFERENCES csi4142project.Location(location_key) ON DELETE RESTRICT ON UPDATE CASCADE);
